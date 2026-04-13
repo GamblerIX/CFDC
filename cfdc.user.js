48b9f1025b4f94c8f7949896bee3cff9a91bf942
GamblerIX <210979020+GamblerIX@users.noreply.github.com>
2026-04-13 13:43:44 +0800

    Add CFDC userscript and favicon
    Introduce a new userscript 'CFDC' intended to localize Cloudflare Developers docs into Chinese. Adds cfdc.user.js containing userscript metadata (name, namespace, version, description, author, match URL and icon) with a placeholder script body, and includes favicon.png as the script icon asset.

---
+++ cfdc.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         CFDC
+// @namespace    http://github.com/GamblerIX/
+// @version      2026-04-13
+// @description  将CloudFlare的Developers文档汉化为中文。
+// @author       GamblerIX
+// @match       https://developers.cloudflare.com/
+// @icon          https://www.google.com/s2/favicons?sz=64&domain=cloudflare.com
+// @grant        
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
